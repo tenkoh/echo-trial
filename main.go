13f2a66a9f1741c6b834f7142c087ee9870a5abf
tenkoh <tenkoh@example.com>
2021-10-29 03:58:00 +0900

    graceful shutdown

--- main.go
+++ main.go
@@ -1,7 +1,12 @@
 package main
 
 import (
+	"context"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/labstack/echo/v4"
 )
@@ -11,5 +16,20 @@ func main() {
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, Echo")
 	})
-	e.Logger.Fatal(e.Start(":1323"))
+	go func() {
+		if err := e.Start(":1323"); err != nil && err != http.ErrServerClosed {
+			e.Logger.Fatal("shutting down the server")
+		}
+	}()
+
+	quit := make(chan os.Signal, 1)
+	// for Windows: sigterm: kill, sigint: ctrl+C
+	// Notice: for MacOS, control[^] + C
+	signal.Notify(quit, syscall.SIGTERM, syscall.SIGINT, os.Interrupt)
+	<-quit
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	if err := e.Shutdown(ctx); err != nil {
+		e.Logger.Fatal(err)
+	}
 }
